50efc9e874f89ec116cf9788cae717c3515b7417
Eduardo Farias <eduardofarias.prg@gmail.com>
2019-06-14 00:29:01 -0300

    new up api-instagram-clone-nodejs

---
+++ src/config/upload.js
@@ -0,0 +1,12 @@
+const multer = require('multer');
+const path = require('path');
+
+
+module.exports = {
+    storage: new multer.diskStorage({
+        destination: path.resolve(__dirname, '..','..','upload'),
+        filename: function(req, file, cb){
+            cb(null, file.originalname);
+        }
+    })
+}
